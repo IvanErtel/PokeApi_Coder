e87ff7d460680f76dacf00820e93ea327e717329
Ivan Ertel <alanertel2275@gmail.com>
2022-05-10 19:15:53 -0300

    poniendo estilo

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
+
+// declaramos variables
 const poke_container = document.getElementById('poke_container');
 var pokeForm = document.getElementById('pokemon').value;
 const pokeContent = document.getElementById('pokemonContent');
 const pokemon_number=100
 
 
+// recooremos los que nos trae la api
+
 const fetchPokemons = async () => {
     pokeForm = document.getElementById('pokemon').value.toUpperCase();
     
     poke_container.innerHTML = ""
+    
     for(let i = 1; i <= pokemon_number; i++){
         await getPokemon(i)
-
     }
-
-    
 }
 
+// consumimos pokeapi
 
 const getPokemon = async (id) => {
     const url = 'https://pokeapi.co/api/v2/pokemon/' + id.toString() 
@@ -25,6 +28,8 @@ const getPokemon = async (id) => {
     createPokemonCard(pokemon)
 }
 
+// creamos las cards que se mostraran en el DOM
+
 const createPokemonCard = (pokemon) => {
     const { types, sprites} = pokemon
     const name = pokemon.name.toUpperCase();
@@ -32,23 +37,24 @@ const createPokemonCard = (pokemon) => {
     const stats = `</br>${pokemon.stats[0].stat.name}:  ${pokemon.stats[0].base_stat} `
     const id = pokemon.id;
     const pokemonEl = document.createElement('div');
-    // setCardColor(types)
-    // console.log(pokemon.types[0].type.name);
-   
     
+    
+    // creamos un if para que se pueda buscar mediante nombre o ID
+
     if (name == pokeForm || pokeForm == "" || id == pokeForm){
+        
  
         pokemonEl.classList.add('pokemon')
         pokemonEl.classList.add('move')
     
         const pokeInnerHtml = `
-        <div class= 'img-container'>
+        <div class= 'img-container' class= '${type}'>
              <img src= '${sprites.front_default}'/>
              </div>
         <div class='info'>
          <span class='number'>${id}</span>
          <h3 class='name'>${name}</h3>
-         <small class='type'>${type}</small>
+         <small class ="type">${type}</small>
          <small class='type'>${stats}</small>
         </div>  
         `
@@ -57,10 +63,18 @@ const createPokemonCard = (pokemon) => {
         poke_container.appendChild(pokemonEl)
         
     }
+        
+    let colorPKMN = types.map(pkmnCOLOR)
+        function pkmnCOLOR(types){
+            return types.type
+        }
+       
+
+    console.log(type);
+  
     
     
 }
 
-   
 fetchPokemons()
 
