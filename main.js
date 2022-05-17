a184439794895f07992f1fd90ddd80dc05afc76d
Ivan Ertel <alanertel2275@gmail.com>
2022-05-16 21:37:07 -0300

    se agrega paginado de siguiente y anterios

--- main.js
+++ main.js
@@ -3,31 +3,51 @@
 const poke_container = document.getElementById('poke_container');
 var pokeForm = document.getElementById('pokemon').value;
 const pokeContent = document.getElementById('pokemonContent');
-const pokemon_number=30
+const anterior = document.getElementById('btnAnterior');
+const siguiente = document.getElementById('btnSiguiente');
+let pokemon_number=8;
+let desde = 1;
+let hasta = 8;
 
+//botones anterior y despues para pasar de pagina.
+anterior.addEventListener('click', () => {
+        desde -= 9;
+       paginado(desde, hasta);
+})
+siguiente.addEventListener('click', () => {
+    desde += 9;
+    paginado(desde, hasta)
+    ;
+})
 
-// recooremos los que nos trae la api
-
-const fetchPokemons = async () => {
+function paginado(desde, hasta){
+    document.getElementById("btnAnterior").hidden = ((desde < 2));
     pokeForm = document.getElementById('pokemon').value.toUpperCase();
-    
     poke_container.innerHTML = ""
-    
-    for(let i = 1; i <= pokemon_number; i++){
-        await getPokemon(i)
+    for(let i = desde; i<= desde + hasta; i++){
+        getPokemon(i)
     }
 }
-
 // consumimos pokeapi
 
 const getPokemon = async (id) => {
     const url = 'https://pokeapi.co/api/v2/pokemon/' + id.toString() 
     const res = await fetch(url)
     const pokemon = await res.json()
-
     createPokemonCard(pokemon)
 }
+// recooremos los que nos trae la api
 
+const fetchPokemons = async () => {
+    pokeForm = document.getElementById('pokemon').value.toUpperCase();
+    poke_container.innerHTML = ""
+    
+    for(let i = 1; i <= pokemon_number; i++){
+        await getPokemon(i)
+    }
+   
+}
+fetchPokemons()
 // creamos las cards que se mostraran en el DOM
 
 const createPokemonCard = (pokemon) => {
@@ -57,80 +77,29 @@ const createPokemonCard = (pokemon) => {
          <h3 class='name'>${name}</h3>
          <small class ="type">${type}</small>
          <small class='type'>${stats}</small>
-         <button type="button" onclick="infoPokemon()" class="btn">Info</button>
         </div>  
         `
-        
-        
-        
-        
         pokemonEl.innerHTML = pokeInnerHtml
         poke_container.appendChild(pokemonEl)
         
         }
-        if(basePokemon.includes("pikachu")){
-            console.log("estoy en la lista");
-            console.log(basePokemon);
+        if(pokemonEl.innerHTML == ""){
+            Swal.fire({
+                        title: 'Lo sentimos!',
+                        text: 'El pokemon que buscas no esta en nuestra lista.',
+                        imageUrl: 'https://sm.ign.com/t/ign_latam/screenshot/default/pikachu-triste_auuu.1280.jpg',
+                        imageWidth: 400,
+                        imageHeight: 200,
+                        imageAlt: 'Custom image',
+                      })
         }
-        // else{
-        //     Swal.fire({
-        //                 title: 'Lo sentimos!',
-        //                 text: 'El pokemon que buscas no esta en nuestra lista.',
-        //                 imageUrl: 'https://sm.ign.com/t/ign_latam/screenshot/default/pikachu-triste_auuu.1280.jpg',
-        //                 imageWidth: 400,
-        //                 imageHeight: 200,
-        //                 imageAlt: 'Custom image',
-        //               })
-        // }
     }
     
     
-        // if(pokeForm == 0){
-        //     Swal.fire('Any fool can use a computer')
-        //     console.log(pokeForm);}
-        
-        
-    // }else{
-    //     Swal.fire('Any fool can use a computer')
-    // }
-    // function infoPokemon(){
-    //     const pokeInnerHtml2 = document.createElement('div');
-    //     pokeInnerHtml2.classList.add('pokemon');
-    //     pokeInnerHtml2.classList.add('move');
-    //     pokeInnerHtml2.classList.add(classType);
-    //     const pokeInfo = `
-    //     <div class= 'img-container2'>
-    //      <span class='card2'>${id}</span>
-    //      <h3 class='card2'>${name}</h3>
-    //      <small class ="card2">${type}</small>
-    //      <small class='card2'>${stats}</small>
-    //      </br>
-    //      <small class='card2'>${pokemon.stats[1].stat.name} ${pokemon.stats[1].base_stat}</small>
-    //      </br>
-    //      <small class='card2'>${pokemon.stats[2].stat.name} ${pokemon.stats[2].base_stat}</small>
-    //      <p class='card2'>Ataques</p>
-    //      <small class='card2'>${pokemon.abilities[0].ability.name},</small>
-    //      <small class='card2'>${pokemon.abilities[1].ability.name}</small>
-    //      </br>
-    //      </div>  
-    //      `
-    //     pokeInnerHtml2.innerHTML = pokeInfo
-    //     poke_container.appendChild(pokeInnerHtml2)
-
-    // }
-    // infoPokemon()
     
-        
-    
-    // let colorPKMN = types.map(pkmnCOLOR)
-    //     function pkmnCOLOR(types){
-    //         return types.type
-    //     }
        
 
-    // console.log(type);
 
 
 
-fetchPokemons()
 
